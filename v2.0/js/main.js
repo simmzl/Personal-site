73d7cdebe8ab4c755740bd49061e173c6919868f
simmzl <czl008@outlook.com>
2019-03-21 21:53:08 +0800

    2019 change

--- v2.0/js/main.js
+++ v2.0/js/main.js
@@ -1,109 +1,34 @@
-const list = document.getElementById('list');
-const backMsg = document.getElementById("backMsg");
-// const more = document.getElementById('more');
-const close = document.getElementById('close');
-const mask = document.getElementById('mask');
-// const modal = document.getElementById('modal');
-const detail = document.getElementById('detail');
-const workshopTitle = document.getElementById('workshopTitle');
-const workshop = document.getElementById('workshop');
-const container = document.getElementById('container');
-
-container.addEventListener('mouseover', e => {
-    const el = e.target || e.srcElement;
-    const name = el.innerHTML.toLowerCase();
-    let backMsgInner;
-    const change = _ => {
-        el.style.color = "#fff";
-        backMsg.innerHTML = backMsgInner;
-        document.body.classList.add(name);
-    };
-
-    switch (name) {
-        case 'cqmovie':
-            backMsgInner = "FOR RECOMMENDING MOVIES I LIKE";
-            change();
-            break;
-        case 'github':
-            backMsgInner = "INTENDING WEB FRONT-END DEVELOPER";
-            change();
-            break;
-        case 'photos':
-            backMsgInner = "PHOTOGRAPH BY @SIMMZL";
-            change();
-            break;
-        case 'blog':
-            backMsgInner = "TO RECORD THE PROBLEMS,TECHNIQUES AND IDEAS";
-            change();
-            break;
-        case 'workshop':
-            backMsgInner = "SOME OF THE PROJECTS I'VE DONE";
-            change();
-            break;
-    }
-});
-
-const back = obj => {
-    const clsName = document.body.className;
-    !!clsName && document.body.classList.remove(clsName);
-    backMsg.innerHTML = "";
-    obj.style.color = "#999";
-    const opacity = detail.style.opacity;
-    if (opacity === "1" && obj.id === "workshop") {
-        detail.style.opacity = "0";
-    }
+const list = document.getElementById("list");
+const banner = document.getElementById("banner");
+const slogans = {
+    cqmovie: "FOR RECOMMENDING MOVIES I LIKE",
+    github: "BE A WONDERFUL FRONT-END DEVELOPER",
+    photos: "PHOTOGRAPH BY @SIMMZL",
+    blog: "TO RECORD THE PROBLEMS,TECHNIQUES AND IDEAS",
+    workshop: "CREATIVE & STYLISH"
 };
 
-workshopTitle.addEventListener("mouseenter", _ => {
-    detail.style.opacity = "1";
-});
-workshopTitle.addEventListener("mouseleave", _ => {
-    workshopTitle.style.color = "#999";
-});
-workshop.addEventListener('mouseleave', _ => {
-    back(workshop);
-});
+const change = (el, slogan, clsName) => {
+    el.style.color = "#fff";
+    banner.innerHTML = slogan;
+    document.body.classList.add(clsName);
+};
 
-list.addEventListener('mouseout', e => {
+const recover = el => {
+    const clsName = document.body.className;
+    !!clsName && document.body.classList.remove(clsName);
+    banner.innerHTML = "";
+    el.style.color = "#999";
+};
+
+list.addEventListener("mouseover", e => {
     const el = e.target || e.srcElement;
-    el.className === 'list' && back(el);
+    const clsName = el.innerHTML.toLowerCase();
+    const slogan = slogans[clsName];
+    el.tagName.toLowerCase() === "a" && change(el, slogan, clsName);
 });
 
-// const showModal = _ => {
-//     back(workshop);
-//     mask.style.display = 'inline';
-//     modal.style.padding = '15px';
-//     modal.style.height = '278px';
-//     modal.style.borderRadius= "16px";
-// };
-// const closeModal = _ =>{
-//     mask.style.display = 'none';
-//     modal.style.height = '0';
-//     modal.style.padding = '0';
-//     modal.style.borderRadius= "0";
-// };
-
-// close.addEventListener('click', closeModal);
-// mask.addEventListener('click', closeModal);
-
-detail.addEventListener('click', e => {
+list.addEventListener("mouseout", e => {
     const el = e.target || e.srcElement;
-    if(el.nodeName.toLowerCase() === 'a') {
-        const opacity = detail.style.opacity;
-        if(!opacity || opacity === '0') {
-            el.className = 'cursorDef';
-            e.preventDefault();
-            // more.removeEventListener('click', showModal);
-        } else {
-            el.className = '';
-            // el.id === 'more' && showModal();
-        }
-    }
-});
-detail.addEventListener('mouseover', e =>{
-    const el = e.target || e.srcElement;
-    if(el.nodeName.toLowerCase() === 'a') {
-        const opacity = detail.style.opacity;
-        !opacity || opacity === '0'? el.className = 'cursorDef' : el.className = '';
-    }
+    el.tagName.toLowerCase() === "a" && recover(el);
 });
